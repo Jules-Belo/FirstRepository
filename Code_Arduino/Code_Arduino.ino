04123039901cc26a6d321fa9160a109c5ceeee47
Jules-Belo <jules.belo@icloud.com>
2025-11-24 17:06:58 +0100

    Étape ajout code arduino 8N1

--- Code_Arduino/Code_Arduino.ino
+++ Code_Arduino/Code_Arduino.ino
@@ -3,7 +3,7 @@ bool startLoop = false;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);    // En attente de commande
-  Serial.begin(1000000);          // Doit matcher le Python
+  Serial.begin(1000000, SERIAL_8N1);          // Doit matcher le Python
   while(!startLoop) {
     if(Serial.available()) {
       startLoop = ((char)(Serial.read()) == 'r');
